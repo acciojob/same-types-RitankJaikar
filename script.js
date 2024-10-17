a7c8e231fbdb8147bee4fb1d956e8d4a8eaca2c8
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-18 05:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function isSameType(value1, value2) {
-	if(typeof(value1)  === typeof(value2)) {
+	// Convert prompt input to actual numbers if applicable
+	const num1 = parseFloat(value1);
+	const num2 = parseFloat(value2);
+	
+	// Check if both are NaN
+	if (Number.isNaN(num1) && Number.isNaN(num2)) {
 		return true;
 	}
 
+	// Check if both are numbers after conversion
+	if (!Number.isNaN(num1) && !Number.isNaN(num2)) {
+		return true;
+	}
+
+	// Otherwise, check if both values have the same type (as strings, objects, etc.)
+	if (typeof value1 === typeof value2) {
+		return true;
+	}
+	
+	// Return false if none of the above conditions are met
 	return false;
 }
 
